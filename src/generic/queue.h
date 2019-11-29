e54a4fea58d93c148025eb9751210a5a5bd3968f
wulkanat <wulkanat@gmail.com>
2019-11-29 17:19:08 +0100

    add queue.h

---
+++ src/generic/queue.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#define QUEUE(name, type) \
+struct QueueElement##name { \
+    struct QueueElement##name *next; \
+    type *data; \
+}; \
+typedef struct QueueElement##name QueueElement##name; \
+ \
+typedef struct { \
+    QueueElement##name *first; \
+    QueueElement##name *last; \
+} Queue##name; \
+Queue##name name = {NULL, NULL};
+
+#define queue_append(queue, data_ptr) while (0) { \
+QueueElement##queue *___tmp = malloc(sizeof(QueueElement##queue)); \
+___tmp->next = NULL; \
+___tmp->data = data_ptr; \
+queue.last->next = ___tmp; \
+queue.last = ___tmp; \
+};
+
+#define queue_pop(queue, data_ptr) while (0) { \
+data_ptr = queue.first->data; \
+QueueElement##queue *___tmp = queue.first; \
+queue.first = queue.first->next; \
+free(___tmp); \
+};
